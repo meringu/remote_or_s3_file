b1173e86cdcf6615be406d189f3b8cde613c52d3
Henry Muru Paenga <meringu@gmail.com>
2016-12-09 14:28:17 +1300

    Apache v2

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'remote_or_s3_file'
 maintainer       'Henry Muru Paenga'
 maintainer_email 'meringu@gmail.com'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Recource wrapper for remote_file and aws_s3_file'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.6'
